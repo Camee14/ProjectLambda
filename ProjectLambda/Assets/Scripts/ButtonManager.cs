76d284678500ca12ab11dead9a2b7b33a35e23cd
Ross <32176419+Bo55killa@users.noreply.github.com>
2018-04-10 15:18:59 +0100

    Added a main menu and pause menu
    It doesn't look great atm. The pause menu only gets activated with a controller as well but I think thats an easy fix I just can't seem to get it.

---
+++ ProjectLambda/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using InControl;
+
+public class ButtonManager : MonoBehaviour {
+
+    private InputDevice Controller;
+    public GameObject canvas;
+
+    private void Update()
+    {
+        Controller = InputManager.ActiveDevice;
+
+        if (Controller.MenuWasPressed)
+        {
+            if(canvas.activeInHierarchy == false)
+            {
+                canvas.SetActive(true);
+                Time.timeScale = 0;
+            }
+        }
+    }
+
+    public void StartBtnPress(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+    public void ExitBtnPress()
+    {
+        Application.Quit();
+        Debug.Log("You have quit the game");
+    }
+    public void resumeBtnPress()
+    {
+        Time.timeScale = 1;
+        canvas.SetActive(false);
+    }
+
+}
