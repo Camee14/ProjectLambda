d8e9cf2d2f3c6d8975aa7153dc0c8dd56fe88912
Camee14 <camee14@hotmail.co.uk>
2018-04-23 21:03:56 +0100

    mouse is hidden when using controller

---
+++ ProjectLambda/Assets/Scripts/HideMouseCursor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+public class HideMouseCursor : MonoBehaviour {
+
+	void Start () {
+        Cursor.visible = (InputManager.ActiveDevice.Name == "Keyboard & Mouse");
+        InputManager.OnActiveDeviceChanged += deviceChanged;
+	}
+    void deviceChanged(InputDevice active)
+    {
+        Cursor.visible = (active.Name == "Keyboard & Mouse");
+    }
+}
